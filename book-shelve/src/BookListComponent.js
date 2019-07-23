b9c5ef6da7d8e976050387bf9ac82aea341d7ab8
brampijper <brampijper@gmail.com>
2019-07-23 15:12:32 +0200

    Added an unique key for every searchresult

---
+++ book-shelve/src/BookListComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function BookListComponent(props) {
+    return (
+        <div className="center-align">
+            <div className="center-elements">
+                <h1> this is going to be your book stance</h1>                
+            </div>
+        </div>
+    )
+}
+
+export default BookListComponent   
